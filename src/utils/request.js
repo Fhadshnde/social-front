d9974aa83cf2c0baeecfc6569b23439637f0eb48
Fhadshnde <Fhadshnde32@gmail.com>
2025-01-09 23:14:27 +0300

    Update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-    baseURL: "https://social-client-fahad.ddnsfree.com/"
+    baseURL: "https://social-api-fahad.gleeze.com/"
 });
 
 export default request;
